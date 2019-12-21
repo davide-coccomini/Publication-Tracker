0dc774cb7f480c20a9ef04257a5157d97313c9d7
Davide57 <davide5713@gmail.com>
2019-12-21 22:30:54 +0100

    Add empty controllers

---
+++ Java/publication-tracker/src/main/java/frontend/LoginController.java
@@ -0,0 +1,26 @@
+package frontend;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class LoginController implements Initializable {
+    @FXML
+    private Label label;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
